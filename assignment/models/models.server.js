7207067699bb1fe0057682dea62acea48b910f9e
Rashmi Dwaraka <dwaraka.r@husky.neu.edu>
2016-06-10 08:35:36 -0400

    mongodb connection setup for openshift

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -2,7 +2,17 @@
  * Created by Rashmi_Dwaraka on 6/7/2016.
  */
 module.exports=function(){
-  
+
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
+
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+    
     var mongoose = require("mongoose");
     mongoose.connect('mongodb://localhost/cs5610WebDev');
 
