0c4254e964183d7b7fa2b58b588f3a4971fe932c
Rashmi Dwaraka <dwaraka.r@husky.neu.edu>
2016-06-19 18:20:02 -0400

    update on mongodb

--- server.js
+++ server.js
@@ -40,16 +40,3 @@ assignment(app);
 // project(app);
 app.listen(port, ipaddress);
 
-var connectionString = 'mongodb://127.0.0.1:27017/webdev';
-// var connectionString = 'mongodb://localhost/cs5610WebDev';
-
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
-
-var mongoose = require("mongoose");
-mongoose.connect(connectionString);
