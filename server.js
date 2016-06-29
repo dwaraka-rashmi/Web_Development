d5d07d46938fbe25eb9c5f1b4fc89603d40d41fc
Rashmi Dwaraka <dwaraka.r@husky.neu.edu>
2016-06-29 00:00:35 -0400

    update on bugs

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var passport = require('passport');
 
 var app = express();
 
-var connectionString = 'mongodb://127.0.0.1:27017/webdev';
-// var connectionString = 'mongodb://localhost/cs5610WebDev';
+// var connectionString = 'mongodb://127.0.0.1:27017/webdev';
+var connectionString = 'mongodb://localhost/cs5610WebDev';
 
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
+// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+//         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+//         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+//         process.env.OPENSHIFT_APP_NAME;
+// }
 
 // //     // connectionString = ''+ process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
 // //     //     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
